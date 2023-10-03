8a4b36c592eeefc28abb573658f0f02beb37edc4
Josh Archer <jarcher@greenriver.edu>
2023-10-03 12:05:02 -0700

    Wrote the add() and remove() operations for a binary heap.

--- priority-queues/src/queues/PriorityQueue.java
+++ priority-queues/src/queues/PriorityQueue.java
@@ -1,5 +1,11 @@
 package queues;
 
+import java.util.Arrays;
+import java.util.NoSuchElementException;
+
+/**
+ * A priority queue based on a binary heap.
+ */
 public class PriorityQueue implements IPriorityQueue
 {
     private static final int INIT_CAPACITY = 10;
@@ -18,20 +24,114 @@ public class PriorityQueue implements IPriorityQueue
     @Override
     public void add(int element)
     {
+        //what if we run out of space?
+        if (size == heap.length)
+        {
+            resize();
+        }
+
         heap[size] = element;
+
+        //reorder the heap
         swim(size);
+
         size++;
     }
 
-    private void swim(int size)
+    private void resize()
     {
-        //TODO we need to write swim()
+        //double the capacity
+        int[] newHeap = new int[heap.length * 2];
+
+        //copy over all elements
+        for (int i = 0; i < heap.length; i++)
+        {
+            newHeap[i] = heap[i];
+        }
+
+        //then replace the heap
+        heap = newHeap;
+    }
+
+    private void swim(int curIndex)
+    {
+        while (curIndex > 0)
+        {
+            int parIndex = (curIndex - 1) / 2;
+
+            //if out of order
+            if (heap[curIndex] < heap[parIndex])
+            {
+                swap(curIndex, parIndex);
+
+                //move up the tree
+                curIndex = parIndex;
+            }
+            else
+            {
+                break; //short-circuit
+            }
+        }
+    }
+
+    private void swap(int first, int second)
+    {
+        int temp = heap[first];
+        heap[first] = heap[second];
+        heap[second] = temp;
     }
 
     @Override
     public int remove()
     {
-        return 0;
+        //is the heap empty
+        if (size == 0)
+        {
+            throw new NoSuchElementException("The heap is empty");
+        }
+
+        //save my return value and move the last element in the heap to the root
+        int result = heap[0];
+        heap[0] = heap[size - 1];
+        heap[size - 1] = 0;
+
+        size--;
+        sink(0);
+
+        return result;
+    }
+
+    private void sink(int curIndex)
+    {
+        int highestIndexWithChild = (size / 2) - 1;
+
+        //loop while the current index has a child
+        while (curIndex <= highestIndexWithChild)
+        {
+            int leftIndex = 2 * curIndex + 1;
+            int rightIndex = 2 * curIndex + 2;
+
+            //find the smallest child of the current index
+            int smallestIndex = leftIndex;
+            if (rightIndex < size &&
+                heap[leftIndex] > heap[rightIndex]) //is the right child smaller than the left
+            {
+                smallestIndex = rightIndex;
+            }
+
+            //swap if out of order
+            if (heap[curIndex] > heap[smallestIndex])
+            {
+                swap(curIndex, smallestIndex);
+
+                //move down the tree
+                curIndex = smallestIndex;
+            }
+            else
+            {
+                break; //short-circuit - the elements are in order!
+            }
+        }
     }
 
     @Override
@@ -43,18 +143,24 @@ public class PriorityQueue implements IPriorityQueue
     @Override
     public int size()
     {
-        return 0;
+        return size;
     }
 
     @Override
     public boolean isEmpty()
     {
-        return false;
+        return size == 0;
     }
 
     @Override
     public void clear()
     {
 
+    }
+
+    @Override
+    public String toString()
+    {
+        return Arrays.toString(heap);
     }
 }
--- priority-queues/src/test/Main.java
+++ priority-queues/src/test/Main.java
@@ -1,9 +1,28 @@
 package test;
 
+import queues.PriorityQueue;
+
 public class Main
 {
     public static void main(String[] args)
     {
-        System.out.println("Hello world!");
+        PriorityQueue heap = new PriorityQueue();
+        int[] elementsToInsert = {15, 24, 2, 5, 42, 36, 15, 7, 4, 7, 11, 18};
+
+        for (int i = 0; i < elementsToInsert.length; i++)
+        {
+            heap.add(elementsToInsert[i]);
+        }
+
+        System.out.println(heap);
+
+        heap.add(4);
+        System.out.println(heap);
+
+        //loop and remove elements until the heap is empty
+        while (!heap.isEmpty())
+        {
+            System.out.println(heap.remove());
+        }
     }
 }
