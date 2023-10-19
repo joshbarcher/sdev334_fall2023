d8afabe58466742958bcd46125678a89103e1881
Josh Archer <jarcher@greenriver.edu>
2023-10-19 10:29:27 -0700

    Added a recursive version of the algorithm.

--- searching/src/searching/binary/Searching.java
+++ searching/src/searching/binary/Searching.java
@@ -2,6 +2,38 @@ package searching.binary;
 
 public class Searching
 {
+    public static int recursiveBS(int[] array, int search)
+    {
+        return recursiveBS(array, search, 0, array.length - 1);
+    }
+
+    private static int recursiveBS(int[] array, int search,
+                                   int low, int high)
+    {
+        //base case
+        if (low > high)
+        {
+            return -1;
+        }
+
+        int mid = (high + low) / 2;
+        if (array[mid] == search) //base case
+        {
+            //return the index where we found the element
+            return mid;
+        }
+        else if (array[mid] < search)
+        {
+            //low -> mid + 1
+            return recursiveBS(array, search, mid + 1, high);
+        }
+        else
+        {
+            //high -> mid - 1
+            return recursiveBS(array, search, low, mid - 1);
+        }
+    }
+
     public static int binarySearch(int[] array, int search)
     {
         int low = 0;
--- searching/src/test/TestBinarySearch.java
+++ searching/src/test/TestBinarySearch.java
@@ -17,6 +17,6 @@ public class TestBinarySearch
         System.out.println(Arrays.toString(testArray));
 
         //search for an element
-        System.out.println("bs(10): " + Searching.binarySearch(testArray, 10));
+        System.out.println("bs(10): " + Searching.recursiveBS(testArray, 10));
     }
 }
