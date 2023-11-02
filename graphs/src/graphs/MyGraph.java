beb14a860572ab48089a8f312fba4c2d2133e3a7
Josh Archer <jarcher@greenriver.edu>
2023-11-02 10:39:01 -0700

    Finished the "add" methods on our graph class.

--- graphs/src/graphs/MyGraph.java
+++ graphs/src/graphs/MyGraph.java
@@ -8,10 +8,16 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 
+/**
+ * This is an undirected, weighted graph!
+ *
+ * @param <V>
+ * @param <E>
+ */
 public class MyGraph<V, E> implements IGraph<V, E>
 {
     //adjacency lists
-    private Map<V, LinkedList<V>> lists;
+    private Map<V, LinkedList<Edge<V, E>>> lists;
 
     public MyGraph()
     {
@@ -22,7 +28,7 @@ public class MyGraph<V, E> implements IGraph<V, E>
     public boolean addVertex(V vertex)
     {
         //is the vertex already in the graph?
-        if (lists.containsKey(vertex))
+        if (containsVertex(vertex))
         {
             return false; //duplicate vertex!
         }
@@ -35,7 +41,19 @@ public class MyGraph<V, E> implements IGraph<V, E>
     public boolean addEdge(V first, V second, E data,
                            int weight)
     {
-        return false;
+        if (first.equals(second) ||
+            !containsVertex(first) || !containsVertex(second) ||
+            containsEdge(first, second))
+        {
+            return false; //duplicate edge or one of the vertices is missing!
+        }
+
+        //add edge to both adjacency lists
+        Edge<V, E> edge = new Edge<>(first, second, data, weight);
+        lists.get(first).add(edge);
+        lists.get(second).add(edge);
+
+        return true;
     }
 
     @Override
